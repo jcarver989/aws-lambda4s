c7071e3daca31db616bd65c20c2d2ae428c686f1
Josh Carver <jcarver989@gmail.com>
2018-04-20 14:48:56 -0700

    add support for custom authorizers and their claims

--- src/main/scala/lambda4s/Request.scala
+++ src/main/scala/lambda4s/Request.scala
@@ -1,30 +1,33 @@
 package lambda4s
 
 case class Request(
-  resource:              String              = "",
-  path:                  String              = "",
-  httpMethod:            String              = "",
-  headers:               Map[String, String] = Map(),
+  resource:              String                      = "",
+  path:                  String                      = "",
+  httpMethod:            String                      = "",
+  headers:               Map[String, String]         = Map(),
   queryStringParameters: Option[Map[String, String]] = None,
   pathParameters:        Option[Map[String, String]] = None,
   stageVariables:        Option[Map[String, String]] = None,
-  requestContext:        RequestContext      = RequestContext(),
-  body:                  String              = "",
-  isBase64Encoded:       Boolean             = false) {
+  requestContext:        RequestContext              = RequestContext(),
+  body:                  String                      = "",
+  isBase64Encoded:       Boolean                     = false) {
   def as[T <: AnyRef: Manifest](implicit json: JSON): T = {
     json.fromJSON[T](body)
   }
 }
 
 case class RequestContext(
-  accountId:    String   = "",
-  resourceId:   String   = "",
-  stage:        String   = "",
-  requestId:    String   = "",
-  identity:     Identity = Identity(),
-  resourcePath: String   = "",
-  httpMethod:   String   = "",
-  apiId:        String   = "")
+  accountId:    String                 = "",
+  resourceId:   String                 = "",
+  stage:        String                 = "",
+  requestId:    String                 = "",
+  identity:     Identity               = Identity(),
+  resourcePath: String                 = "",
+  httpMethod:   String                 = "",
+  apiId:        String                 = "",
+  authorizer:   Option[AuthorizerInfo] = None)
+
+case class AuthorizerInfo(claims: Map[String, String])
 
 case class Identity(
   cognitoIdentityPoolId:         String = "",
